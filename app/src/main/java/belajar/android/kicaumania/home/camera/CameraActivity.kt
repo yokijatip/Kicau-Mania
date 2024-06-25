c53633a5c65925b5f4fe493ec45ca5bbf1daf711
Yoki Jati Perkasa <yokijati@gmail.com>
2024-06-26 01:35:23 +0700

    Berhasil menyalakan kamera dan membuka gallery

--- app/src/main/java/belajar/android/kicaumania/home/camera/CameraActivity.kt
+++ app/src/main/java/belajar/android/kicaumania/home/camera/CameraActivity.kt
@@ -1,16 +1,92 @@
 package belajar.android.kicaumania.home.camera
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import belajar.android.kicaumania.R
 import belajar.android.kicaumania.databinding.ActivityCameraBinding
+import belajar.android.kicaumania.home.camera.OpenCameraActivity.Companion.CAMERAX_RESULT
 
 class CameraActivity : AppCompatActivity() {
 
+    companion object {
+        private const val REQUIRED_PERMISSION_CAMERA = Manifest.permission.CAMERA
+    }
+
     private lateinit var binding: ActivityCameraBinding
+    private var currentImageUri: Uri? = null
+
+    //    Permission Camera START
+    private fun allPermissionGranted() = ContextCompat.checkSelfPermission(
+        this, REQUIRED_PERMISSION_CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private val requestPermissionLauncer =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this@CameraActivity, "Perizinan diijinkan", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(this@CameraActivity, "Perizinan ditolak", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERAX_RESULT) {
+            currentImageUri =
+                it.data?.getStringExtra(OpenCameraActivity.EXTRA_CAMERAX_IMAGE)?.toUri()
+            convertImageToBitmap()
+        }
+    }
+
+    private fun startCameraX() {
+        val intent = Intent(this@CameraActivity, OpenCameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+    //    Permission Camera END
+
+    //    Permission Gallery START
+    private val launcherGallery =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri: Uri? ->
+            if (uri != null) {
+                currentImageUri = uri
+                //  show image
+                convertImageToBitmap()
+            } else {
+                Log.d("IMAGE URI", "Photo Picker : No Media Selected")
+            }
+        }
+
+    private fun startGallery() {
+        launcherGallery.launch(
+            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+        )
+    }
+
+    private fun convertImageToBitmap() {
+        currentImageUri?.let {
+            val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(it))
+            binding.ivManuk.setImageBitmap(bitmap)
+            //  Prediction nya simpan disini dan dijalankan disini
+        }
+    }
+    //    Permission Gallery END
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityCameraBinding.inflate(layoutInflater)
@@ -22,5 +98,26 @@ class CameraActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+//        Permission All Granted Handle
+        if (!allPermissionGranted()) {
+            requestPermissionLauncer.launch(REQUIRED_PERMISSION_CAMERA)
+        }
+
+        binding.apply {
+            btnOpenCamera.setOnClickListener {
+                startCameraX()
+            }
+
+            btnGallery.setOnClickListener {
+                startGallery()
+            }
+
+            btnBack.setOnClickListener {
+                finish()
+            }
+        }
     }
+
+
 }
--- app/src/main/java/belajar/android/kicaumania/home/camera/OpenCameraActivity.kt
+++ app/src/main/java/belajar/android/kicaumania/home/camera/OpenCameraActivity.kt
@@ -1,16 +1,34 @@
 package belajar.android.kicaumania.home.camera
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import belajar.android.kicaumania.R
+import belajar.android.kicaumania.core.utils.Tools
 import belajar.android.kicaumania.databinding.ActivityOpenCameraBinding
+import java.util.concurrent.ExecutorService
 
 class OpenCameraActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityOpenCameraBinding
+
+    //    Camera X
+    private var imageCapture: ImageCapture? = null
+    private var currentImage: Uri? = null
+    private lateinit var cameraExecutor: ExecutorService
+    private var cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityOpenCameraBinding.inflate(layoutInflater)
@@ -22,5 +40,95 @@ class OpenCameraActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.apply {
+            changeCamera.setOnClickListener {
+                cameraSelector =
+                    if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA
+                    else CameraSelector.DEFAULT_BACK_CAMERA
+                startCamera()
+            }
+
+            imageCapture.setOnClickListener { takePhoto() }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startCamera()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    //    Start Camera X
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
+            } catch (e: Exception) {
+                Toast.makeText(this@OpenCameraActivity, "Gagal Membuka Kamera", Toast.LENGTH_SHORT)
+                    .show()
+                Log.e(TAG, "startCamera: ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    //     Take Photo
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = Tools().createCustomTempFile(application)
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(
+                        this@OpenCameraActivity,
+                        "Gambar berhasil diambil",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    val intent = Intent()
+                    intent.putExtra(EXTRA_CAMERAX_IMAGE, outputFileResults.savedUri.toString())
+                    setResult(CAMERAX_RESULT, intent)
+                    finish()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        this@OpenCameraActivity,
+                        "Gambar gagal diambil",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.d(TAG, "onError: ${exception.message}")
+                }
+
+            }
+        )
+    }
+
+    companion object {
+        private const val TAG = "OpenCameraActivity"
+
+        //        Keperluan untuk menyimpan photo dan melihat dan juga mengirim photo ke activity yang dituju
+        const val EXTRA_CAMERAX_IMAGE = "Camera Image"
+        const val CAMERAX_RESULT = 200
     }
 }
